65d424f75f711e05963e1a04703702922a75a7d6
Rob Duarte <me@robduarte.com>
2022-11-01 09:16:33 -0400

    Complete demo

---
+++ card.js
@@ -0,0 +1,24 @@
+class Card {
+  constructor(img,x,y) {
+    this.img = img;
+    this.x = x;
+    this.y = y;
+    this.w = 72;
+    this.h = 131;
+    this.isHidden = false;
+  }
+  isInside(xPos,yPos) {
+    if (xPos > this.x && xPos < this.x+this.w && yPos > this.y && yPos < this.y+this.h )
+      return true;
+    else
+      return false;
+  }
+  display() {
+    if (!this.isHidden) {
+      stroke("white");
+      strokeWeight(10)
+      rect(this.x,this.y,this.w,this.h,5);
+      image(this.img, this.x, this.y);
+    }
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+let cards = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 400);
+  for (let i = 0; i <= 5; i++) {
+    let thisImg = loadImage("assets/card"+i+".png");
+    let thisCard = new Card(thisImg, 50+i*100, height/2);
+    cards.push(thisCard);
+  }
 }
 
 function draw() {
   background(125);
+  cards.forEach(element => element.display());
+}
+
+function mouseClicked() {
+  for (let i = 0; i < cards.length; i++) {
+    const element = cards[i];
+    if (element.isInside(mouseX,mouseY))
+      element.isHidden = true;
+  }
+}
+
+function keyPressed() {
+  cards.forEach(element => element.isHidden=false)
+  cards = shuffle(cards); 
+  // just shuffling the array isn't enough to change how they
+  // are ordered on screen, since each card has its x and y values
+  for (let i = 0; i < cards.length; i++) {
+    const element = cards[i];
+    element.x = 50+i*100;
+  }
 }
