a3766b01367816a51430b01fd5ece1af822fadcb
Rob Duarte <me@robduarte.com>
2022-11-01 12:05:59 -0400

    couple tweaks based on demo video

--- sketch.js
+++ sketch.js
@@ -1,35 +1,35 @@
-let cards = [];
+let deck = [];
 
 function setup() {
   createCanvas(200, 1000);
   for (let i = 0; i <= 5; i++) {
     let thisImg = loadImage("assets/card"+i+".png", img => {
       let thisCard = new Card(thisImg, 40, 40+i*150);
-      cards.push(thisCard);
+      deck.push(thisCard);
     });
   }
 }
 
 function draw() {
   background(125);
-  cards.forEach(element => element.display());
+  deck.forEach(element => element.display());
 }
 
 function mouseClicked() {
-  for (let i = 0; i < cards.length; i++) {
-    const element = cards[i];
+  for (let i = 0; i < deck.length; i++) {
+    const element = deck[i];
     if (element.isInside(mouseX,mouseY))
       element.isHidden = true;
   }
 }
 
 function keyPressed() {
-  cards.forEach(element => element.isHidden=false)
-  cards = shuffle(cards); 
+  deck = shuffle(deck); 
   // just shuffling the array isn't enough to change how they
   // are ordered on screen, since each card has its x and y values
-  for (let i = 0; i < cards.length; i++) {
-    const element = cards[i];
+  for (let i = 0; i < deck.length; i++) {
+    const element = deck[i];
+    element.isHidden = false;
     element.y = 40+i*150;
   }
 }
